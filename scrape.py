87c25f46cf82c46a06c97f941eeb1e6c98c37105
github-jl14 <alt444.001@gmail.com>
2024-12-19 09:15:49 +0800

    '/' made errors

--- scrape.py
+++ scrape.py
@@ -14,7 +14,7 @@ def format_date(postdate_text):
 
 # Function to scrape editorial articles
 def scrape_editorial_articles(url):
-    coverPath = "/covers"
+    coverPath = "covers"
   
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
